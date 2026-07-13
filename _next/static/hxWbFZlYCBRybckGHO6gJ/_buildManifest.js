365a68366f888f6e15c157ca9d3a1b5fc3c6adcb
yufy <aassssddd243@gmail.com>
2026-07-13 11:23:51 +0900

    SNSドリル 最新版 静的ビルド公開

---
+++ _next/static/hxWbFZlYCBRybckGHO6gJ/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/sns-drill-live//_next/:path+",
+        "destination": "/sns-drill-live/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
